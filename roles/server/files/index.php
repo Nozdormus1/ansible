1a7ac9b10eaa4474ee19213c050a4c5def80257d
neifn <alexandrshahmatov@gmail.com>
2017-08-23 02:00:16 +0300

    Added php to mysql page

--- roles/server/files/index.php
+++ roles/server/files/index.php
@@ -1,3 +1,45 @@
+<!Doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="description" content="$1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<title>test</title>
+
+
+</head>
+<body>
+
 <?php
-phpinfo();
+  $conn=mysqli_connect("10.0.1.13","task_user","hard_password","task_db");
+
+  if(!$conn)
+  {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+
+  if(isset($_POST['save']))
+  {
+    $sql = "INSERT INTO pet (date, text)
+    VALUES (now(),'".$_POST["text"]."')";
+
+    $result = mysqli_query($conn,$sql);
+  }
+
 ?>
+
+<form action="index.php" method="post">
+<label id="first"> Text:</label><br/>
+<input type="text" name="text"><br/>
+
+<button type="submit" name="save">save</button>
+
+</form>
+
+</body>
+</html>
